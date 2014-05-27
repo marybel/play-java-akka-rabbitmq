8b13897e222c663f2cb170b273e7c73c3140dc83
Marybel Archer <marybel.archer@bancvue.com>
2014-05-27 15:44:05 -0500

    Setup publish and subscribe scenario

--- app/infrastructure/Messaging.scala
+++ app/infrastructure/Messaging.scala
@@ -39,42 +39,50 @@ object Sender {
   def startSending = {
     // create the connection
     val connection = RabbitMQConnection.getConnection()
-    // create the channel we use to send
-    val sendingChannel = connection.createChannel()
-    // make sure the queue exists we want to send to
-    sendingChannel.queueDeclare(Config.RABBITMQ_QUEUE,  true, false, false, null)
+    // create a new sending channel on which we declare the exchange
+    val sendingChannel2 = connection.createChannel();
+    sendingChannel2.exchangeDeclare(Config.RABBITMQ_EXCHANGEE, "fanout");
 
-    Akka.system.scheduler.schedule(2 seconds, 1 seconds
-      , Akka.system.actorOf(Props(
-        new SendingActor(channel = sendingChannel,
-          queue = Config.RABBITMQ_QUEUE)))
-      , "MSG to Queue")
+    // define the two callbacks for our listeners
+    val callback3 = (x: String) => Logger.info("Recieved on exchange callback 3: " + x);
+    val callback4 = (x: String) => Logger.info("Recieved on exchange callback 4: " + x);
 
-    val callback1 = (x: String) => Logger.info("Recieved on queue callback 1: " + x)
+    // create a channel for the listener and setup the first listener
+    val listenChannel1 = connection.createChannel();
+    setupListener(listenChannel1,listenChannel1.queueDeclare().getQueue(),
+      Config.RABBITMQ_EXCHANGEE, callback3);
 
-    setupListener(connection.createChannel(),Config.RABBITMQ_QUEUE, callback1)
+    // create another channel for a listener and setup the second listener
+    val listenChannel2 = connection.createChannel();
+    setupListener(listenChannel2,listenChannel2.queueDeclare().getQueue(),
+      Config.RABBITMQ_EXCHANGEE, callback4);
 
-    // create an actor that starts listening on the specified queue and passes the
-    // received message to the provided callback
-    val callback2 = (x: String) => Logger.info("Recieved on queue callback 2: " + x)
-
-    // setup the listener that sends to a specific queue using the SendingActor
-    setupListener(connection.createChannel(),Config.RABBITMQ_QUEUE, callback2)
+    // create an actor that is invoked every two seconds after a delay of
+    // two seconds with the message "msg"
+    Akka.system.scheduler.schedule(2 seconds, 1 seconds, Akka.system.actorOf(Props(
+      new PublishingActor(channel = sendingChannel2
+        , exchange = Config.RABBITMQ_EXCHANGEE))),
+      "MSG to Exchange");
   }
 
-  private def setupListener(receivingChannel: Channel, queue: String, f: (String) => Any) {
+  private def setupListener(channel: Channel, queueName : String, exchange: String, f: (String) => Any) {
+    channel.queueBind(queueName, exchange, "");
+
     Akka.system.scheduler.scheduleOnce(2 seconds,
-      Akka.system.actorOf(Props(new ListeningActor(receivingChannel, queue, f))), "")
+      Akka.system.actorOf(Props(new ListeningActor(channel, queueName, f))), "");
   }
 }
 
-class SendingActor(channel: Channel, queue: String) extends Actor {
+class PublishingActor(channel: Channel, exchange: String) extends Actor {
 
+  /**
+   * When we receive a message we sent it using the configured channel
+   */
   def receive = {
     case some: String => {
-      val msg = (some + " : " + System.currentTimeMillis())
-      channel.basicPublish("", queue, null, msg.getBytes())
-      Logger.info(msg)
+      val msg = (some + " : " + System.currentTimeMillis());
+      channel.basicPublish(exchange, "", null, msg.getBytes());
+      Logger.info(msg);
     }
     case _ => {}
   }
